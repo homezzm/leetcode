f1e83d390de29c41a20db223ee7ba675ab361751
homezzm <homezzm@126.com>
2021-01-22 12:58:39 +0800

    147. 对链表进行插入排序

--- LeetCode/中等/链表/138. 复制带随机指针的链表.py
+++ LeetCode/中等/链表/138. 复制带随机指针的链表.py
@@ -1,11 +1,10 @@
-"""
 # Definition for a Node.
 class Node:
     def __init__(self, x, next=None, random=None):
         self.val = int(x)
         self.next = next
         self.random = random
-"""
+
 
 class Solution(object):
     def copyRandomList(self, head):
@@ -15,3 +14,26 @@ class Solution(object):
         :rtype: Node
         给定一个链表，每个节点包含一个额外增加的随机指针，该指针可以指向链表中的任何节点或空节点。
         """
+
+        dicts = dict()  # 保存所有创建的对象 k是旧节点 v是新节点
+
+        def getNode(oriNode):
+            if oriNode in dicts:
+                return dicts[oriNode]
+            newNode = Node(oriNode.val)
+            dicts[oriNode] = newNode
+            return newNode
+
+        dummy = Node(0)
+        newHead = dummy
+        while head:
+            node = getNode(head)
+            if head.next:
+                node.next = getNode(head.next)
+            if head.random:
+                node.random = getNode(head.random)
+            dummy.next = node
+            dummy = node
+            head = head.next
+
+        return newHead.next
