5401cd9eaf1d31910a84dbd4246f0cf197c5d3ef
homezzm <homezzm@126.com>
2021-01-25 14:58:33 +0800

    面试题 04.03. 特定深度节点链表

---
+++ 面试题&剑指 Offer/面试题 04.03. 特定深度节点链表.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def listOfDepth(self, tree):
+        """
+        :type tree: TreeNode
+        :rtype: List[ListNode]
+        """
