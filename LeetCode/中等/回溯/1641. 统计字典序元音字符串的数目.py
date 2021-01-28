90936f4c4cde232ed4c55bb58ac9e0fea34c2eae
homezzm <homezzm@126.com>
2021-01-28 15:24:53 +0800

    1641. 统计字典序元音字符串的数目

---
+++ LeetCode/中等/回溯/1641. 统计字典序元音字符串的数目.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def countVowelStrings(self, n):
+        """
+        https://leetcode-cn.com/problems/count-sorted-vowel-strings/
+        :type n: int
+        :rtype: int
+        给你一个整数 n，请返回长度为 n 、仅由元音 (a, e, i, o, u) 组成且按 字典序排列 的字符串数量。
+        字符串 s 按 字典序排列 需要满足：对于所有有效的 i，s[i]
+        在字母表中的位置总是与 s[i+1] 相同或在 s[i+1] 之前。
+        输入：n = 2 输出：15
+        解释：仅由元音组成的 15 个字典序字符串为
+        ["aa","ae","ai","ao","au","ee","ei","eo","eu","ii","io","iu","oo","ou","uu"]
+        注意，"ea" 不是符合题意的字符串，因为 'e' 在字母表中的位置比 'a' 靠后
+        """
+
+        res = 0
+        if n <= 0: return res
+        li = ['a', 'e', 'i', 'o', 'u']
+
+        def backtrack(startInx, paths):
+            if len(paths) == n:
+                nonlocal res
+                res += 1
+                return
+
+            for i in range(startInx, len(li)):
+                paths.append(li[i])
+                backtrack(i, paths)
+                paths.pop()
+
+        backtrack(0, [])
+        return res
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.countVowelStrings(2))
--- LeetCode/中等/回溯/40. 组合总和 II.py
+++ LeetCode/中等/回溯/40. 组合总和 II.py
@@ -11,24 +11,50 @@ class Solution(object):
 
         输入: candidates = [10,1,2,7,6,1,5], target = 8,
         所求解集为:[[1, 7],[1, 2, 5],[2, 6],[1, 1, 6]]
+
+        i > startInx and nums[i] == nums[i - 1]
+        这个避免重复当思想是在是太重要了。
+        这个方法最重要的作用是，可以让同一层级，不出现相同的元素。即
+                          1
+                         / \
+                        2   2  这种情况不会发生 但是却允许了不同层级之间的重复即：
+                       /     \
+                      5       5
+                        例2
+                          1
+                         /
+                        2      这种情况确是允许的
+                       /
+                      2
+
+        为何会有这种神奇的效果呢？
+        首先 i-1 == i 是用于判定当前元素是否和之前元素相同的语句。这个语句就能砍掉例1。
+        可是问题来了，如果把所有当前与之前一个元素相同的都砍掉，那么例二的情况也会消失。
+        因为当第二个2出现的时候，他就和前一个2相同了。
+
+        那么如何保留例2呢？
+        那么就用i > startInx 来避免这种情况，你发现例1中的两个2是处在同一个层级上的，
+        例2的两个2是处在不同层级上的。
+        在一个for循环中，所有被遍历到的数都是属于一个层级的。我们要让一个层级中，
+        必须出现且只出现一个2，那么就放过第一个出现重复的2，但不放过后面出现的2。
+        第一个出现的2的特点就是 i == startInx. 第二个出现的2 特点是i > startInx.
         """
         res = []
         if not candidates or target <= 0: return res
-
+        nums=sorted(candidates)
         def backtrack(sumVal, startInx, paths):
             if sumVal == target:
-                paths = sorted(paths)
-                if paths not in res:
-                    res.append(paths[:])
+                res.append(paths[:])
                 return
 
-            for i in range(startInx, len(candidates)):
-                val = sumVal + candidates[i]
+            for i in range(startInx, len(nums)):
+                val = sumVal + nums[i]
                 if val > target:  # 做个剪技
                     break
-                if i > 0 and i > startInx and candidates[i] == candidates[i - 1]:
+                #i==startInx是第一个出现的数字 i>startInx就是第二个出现的数字
+                if i > 0 and i > startInx and nums[i] == nums[i - 1]:
                     continue
-                paths.append(candidates[i])
+                paths.append(nums[i])
                 backtrack(val, i + 1, paths)
                 paths.pop()
 
@@ -38,4 +64,4 @@ class Solution(object):
 
 if __name__ == '__main__':
     solution = Solution()
-    print(solution.combinationSum2([1,1,1,2,3,4,5,5,3,6,1],27))
+    print(solution.combinationSum2([10,1,2,7,6,1,5],8))
